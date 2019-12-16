62f6cad5e3bd6c51ff24777463cb965d6961277d
Sameer <sameer.bhardwaj138@gmail.com>
2019-12-16 19:00:02 +0530

    done

--- src/RootNavigator.js
+++ src/RootNavigator.js
@@ -181,7 +181,7 @@ const MainStack = createStackNavigator(
     },
   },
   {
-    initialRouteName: 'HomeNavigator',
+    initialRouteName: 'SplashScreen',
     defaultNavigationOptions: ({ navigation }) => ({
       headerBackTitle: null,
       header: null,
