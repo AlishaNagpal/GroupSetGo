b1e4c23fd517f7f4cacc6ec8ac65bd445444c2fe
Sameer <sameer.bhardwaj138@gmail.com>
2019-12-18 12:02:15 +0530

    added filter

--- src/RootNavigator.js
+++ src/RootNavigator.js
@@ -196,7 +196,7 @@ const MainStack = createStackNavigator(
     },
   },
   {
-    initialRouteName: 'CreateEventStep2',
+    initialRouteName: 'SplashScreen',
     defaultNavigationOptions: ({ navigation }) => ({
       headerBackTitle: null,
       header: null,
