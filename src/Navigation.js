08e105ed4875fefc78bb9e007a94884e3b2b113e
Sameer <sameer.bhardwaj138@gmail.com>
2019-12-27 17:54:46 +0530

    ok

--- src/Navigation.js
+++ src/Navigation.js
@@ -267,7 +267,7 @@ export default createAppContainer(createSwitchNavigator(
         ModalStack: ModalStack
     },
     {
-        initialRouteName: 'ModalStack',
+        initialRouteName: 'SplashStack',
         headerMode: 'none',
         defaultNavigationOptions: ({ navigation }) => ({
             headerBackTitle: null,
